a23662c385e8986ad51df016cf4974116ddee348
majian <majian_happy@613.com>
2012-09-19 18:22:49 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by MaJian on 12-9-19.
+//  Copyright (c) 2012年 MaJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by MaJian on 12-9-19.
+//  Copyright (c) 2012年 MaJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
